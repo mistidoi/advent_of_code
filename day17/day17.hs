d73267dd79b2e8ae6444a1f3adff29fd71149818
mistidoi <brandon.r.hamilton@gmail.com>
2016-08-15 00:35:58 -0400

    rename allCombinations to powerSet, refactor and add explanation

--- day17/day17.hs
+++ day17/day17.hs
@@ -3,7 +3,7 @@ import Data.List
 main = do
     let input = [33,14,18,20,45,35,16,35,1,13,18,13,50,44,48,6,24,41,30,42]
     let total = 150
-    let combinations_summing_to_total = [x | x <- allCombinations input, sum x == total]
+    let combinations_summing_to_total = [x | x <- powerSet input, sum x == total]
     putStrLn "Solution to part 1:"
     print $ length $ combinations_summing_to_total
 
@@ -12,11 +12,19 @@ main = do
     putStrLn "Solution to part 2:"
     print $ length solutions_with_minimum
 
-allCombinations :: [a] -> [[a]]
-allCombinations [] = [[]]
-allCombinations [x] = [[x]]
-allCombinations (x:xs) = (appendToEach x (allCombinations xs)) ++ allCombinations xs ++ [[x]]
+powerSet :: [a] -> [[a]]
+powerSet [x] = [[x]]
+powerSet (x:xs) = [[x]] ++ map (x:) (powerSet xs) ++ powerSet xs
 
-appendToEach :: a -> [[a]] -> [[a]]
-appendToEach number_to_be_added array_of_arrays = map (\y -> number_to_be_added:y) array_of_arrays
+-- I've since learned that this is called a power set.
+--
+-- Well actually, the power set includes the empty set, but it
+-- probably still would have helped me a lot if I knew what it was
+-- called.
+--
+-- In English, the power set of a list of elements
+-- can be defined recursively as:
+-- the list containing first element of the list
+-- plus the solution to the rest of the list
+-- plus a copy of the solution to the rest of the list with the first element appended to each previous subset.
 
